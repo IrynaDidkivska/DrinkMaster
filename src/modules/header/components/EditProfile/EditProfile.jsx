e271cb755f6fb068528301f42ca9fbadd0fd8f46
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-11-16 23:11:40 +0100

    add thunk singnin

--- src/modules/header/components/EditProfile/EditProfile.jsx
+++ src/modules/header/components/EditProfile/EditProfile.jsx
@@ -1,15 +1,26 @@
-import profileFoto from '../../images/profile-foto.png';
+import { getUser } from "../../../../redux/Auth/selectors";
+import profileFoto from "../../images/profile-foto.png";
 import {
   StyledBtnProfile,
   StyledImgProfile,
   StyledProfileName,
-} from './EditProfile';
+} from "./EditProfile";
+import { useSelector } from "react-redux";
 
 export const EditProfile = () => {
+  const editString = (name) => {
+    if (name) {
+      const arrFromStr = name.split(" ");
+      const res = arrFromStr.map((el) => el[0].toUpperCase() + el.slice(1));
+      return res.join(" ");
+    }
+  };
+  const { email } = useSelector(getUser);
+  console.log(email);
   return (
     <StyledBtnProfile>
       <StyledImgProfile src={profileFoto} alt="profile foto" />
-      <StyledProfileName>Victoria</StyledProfileName>
+      <StyledProfileName>{editString(email)}</StyledProfileName>
     </StyledBtnProfile>
   );
 };
