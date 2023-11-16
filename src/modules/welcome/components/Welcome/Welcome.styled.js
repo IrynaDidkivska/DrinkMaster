9b76d4722c93ee3fe86e91838be107f63548a039
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-11-16 13:17:12 +0100

    updated links

--- src/modules/welcome/components/Welcome/Welcome.styled.js
+++ src/modules/welcome/components/Welcome/Welcome.styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { AuthLink } from "../../../../shared/components/StyledLink/StyledLink.styled";
 
 export const StyledWrapper = styled.div`
   display: flex;
@@ -77,39 +76,6 @@ export const StyledLinkWrapp = styled.div`
   justify-content: center;
   gap: 14px;
   margin-top: 40px;
-`;
-
-// Welcome Page Sign Links
-
-export const SignUp = styled(AuthLink)`
-  color: ${({ theme }) => theme.button.dark};
-  background: ${({ theme }) => theme.button.ligth};
-
-  &:hover,
-  &:focus {
-    border: 1px solid rgba(243, 243, 243, 0.2);
-
-    color: ${({ theme }) => theme.button.ligth};
-    background: ${({ theme }) => theme.button.dark};
-    transition: ${({ theme }) => theme.transition};
-  }
-`;
-
-export const SignIn = styled(AuthLink)`
-  border: 1px solid rgba(243, 243, 243, 0.2);
-  color: ${({ theme }) => theme.colors.mainText};
-  background: transparent;
-
-  &:hover,
-  &:focus {
-    cursor: pointer;
-    transition: ${({ theme }) => theme.transition};
-  }
-  &:active {
-    border: none;
-    color: ${({ theme }) => theme.colors.mainText};
-    background: ${({ theme }) => theme.background.dropdown};
-  }
 `;
 
 // Overlay
