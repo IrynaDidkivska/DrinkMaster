625348545fd91205a8a4a80e4f6cdc464626651b
Olga-Olga <olga_l@ukr.net>
2023-11-18 15:37:02 +0200

    successfullSignUp

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,8 +1,11 @@
+import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { signupThunk } from "../../redux/Auth/operations";
 import { selectIsLoading, selectUser } from "../../redux/Auth/selectors";
+import React, { useState } from "react";
 
 import {
   StyledCalendarSvg,
@@ -20,10 +23,9 @@ import Subtitle from "../../shared/components/Title/Subtitle";
 import { isValidDate } from "../../shared/helpers/isValidDate";
 
 const SignUp = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
-
-  const { username, password, email, birthdate } = useSelector(selectUser);
 
   const formik = useFormik({
     initialValues: {
@@ -34,19 +36,37 @@ const SignUp = () => {
     },
     validationSchema: Yup.object({
       username: Yup.string().required("Name is required"),
-      birthdate: Yup.date().required("Date of Birth is required"),
+      // birthdate: Yup.date().required("Date of Birth is required"),
       email: Yup.string()
         .email("Invalid email address")
         .required("Email is required"),
       password: Yup.string()
         .min(6, "Password must be at least 6 characters")
+        .max(20, "Password must be less then 20 characters")
         .required("Password is required"),
     }),
     onSubmit: (values) => {
       // Handle form submission logic here
       console.log(values);
-      const credentials = { username, password, email, birthdate };
-      dispatch(signupThunk(credentials));
+      const credentials = {
+        username: values.username,
+        birthdate: values.birthdate,
+        password: values.password,
+        email: values.email,
+      };
+      dispatch(signupThunk(credentials))
+        .unwrap()
+        .then(() => {
+          navigate("/"); // Redirect to /home after successful login
+        })
+        .catch((error) => {
+          const { message } = error.response.credentials;
+          if (message.length > 5) {
+            toast.error(message);
+          } else {
+            message.forEach((el) => toast.error(el));
+          }
+        });
     },
   });
 
@@ -57,6 +77,14 @@ const SignUp = () => {
         value: value.format("YYYY-MM-DD"),
       },
     });
+  };
+
+  const datetimeOptions = {
+    timeFormat: false,
+    closeOnSelect: true,
+    isValidDate: (current) => {
+      return current.isBefore();
+    },
   };
 
   return (
@@ -75,7 +103,6 @@ const SignUp = () => {
         {formik.touched.username && formik.errors.username ? (
           <div>{formik.errors.username}</div>
         ) : null}
-
         <StyledDatatimeWrapper>
           <StyledDatetime
             type="date"
@@ -87,12 +114,13 @@ const SignUp = () => {
             value={formik.values.birthdate}
             isValidDate={isValidDate}
             closeOnSelect={true}
+            {...datetimeOptions}
           />
+
           <StyledCalendarSvg>
             <SpriteSVG name={"calendar"} />
           </StyledCalendarSvg>
         </StyledDatatimeWrapper>
-
         {formik.touched.birthdate && formik.errors.birthdate ? (
           <div>{formik.errors.birthdate}</div>
         ) : null}
@@ -109,7 +137,6 @@ const SignUp = () => {
         {formik.touched.email && formik.errors.email ? (
           <div>{formik.errors.email}</div>
         ) : null}
-
         <input
           type="password"
           id="password"
