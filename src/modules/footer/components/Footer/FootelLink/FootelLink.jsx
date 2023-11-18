41d6985ed8d41ea0a000f291f60e7373978c5112
Ihor <fedonn@ukr.net>
2023-11-18 11:58:55 +0200

    Finaly update styled & Links

---
+++ src/modules/footer/components/Footer/FootelLink/FootelLink.jsx
@@ -0,0 +1,14 @@
+import { StyledLinkFooter, StyledNavFooter } from './FootelLink.styled'
+
+const FootelLink = () => {
+	return (
+		<StyledNavFooter>
+			<StyledLinkFooter>Drinks</StyledLinkFooter>
+			<StyledLinkFooter>Add drink</StyledLinkFooter>
+			<StyledLinkFooter>My drinks</StyledLinkFooter>
+			<StyledLinkFooter>Favorites drinks</StyledLinkFooter>
+		</StyledNavFooter>
+	)
+}
+
+export default FootelLink
