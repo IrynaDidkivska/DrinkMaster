3c622d820418dc140b23407d4a61355eb3703eb1
Olga-Olga <olga_l@ukr.net>
2023-11-19 17:17:51 +0200

    redirect on CardId

--- src/shared/components/DrinkCardItem/DrinkCardItem.jsx
+++ src/shared/components/DrinkCardItem/DrinkCardItem.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import DrinkFavImage from "../../components/DrinkCardItem/Rectangle40559.png";
 import {
   DrinkCardItemContainer,
@@ -8,12 +9,18 @@ import {
 } from "./DrinkCardItem.styled";
 
 const DrinkCartItem = ({ _id, drinkThumb, drink }) => {
+  const navigate = useNavigate();
+
+  const handleSeeMore = (_id) => {
+    navigate(`/drinks/${_id}`);
+  };
+
   return (
     <DrinkCardItemContainer key={_id}>
       <DrinkCardItemImage src={drinkThumb} alt={drink} />
       <DrinkCardItemContainerDiscr>
         <DrinkCardItemName>{drink}</DrinkCardItemName>
-        <DrinkCardItemSeeMore href="редірект ту Май Дрінк">
+        <DrinkCardItemSeeMore onClick={() => handleSeeMore(_id)}>
           See more
         </DrinkCardItemSeeMore>
       </DrinkCardItemContainerDiscr>
