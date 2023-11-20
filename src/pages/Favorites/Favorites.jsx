f7de3d99f926d1df493a0d598781c66a634a4857
Ihor <fedonn@ukr.net>
2023-11-21 01:24:42 +0200

    update FavNotAdd

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,13 +1,11 @@
 import { DrinkListID } from '../../shared/components/DrinkList/DrinkListId'
 import Title from '../../shared/components/Title/Title'
-import { FavNotAdd } from './FavNotAdd/FavNotAdd'
 
 const DrinksListFav = () => {
 	return (
 		<>
 			<Title Title='Favorites' />
 			<DrinkListID />
-			<FavNotAdd />
 		</>
 	)
 }
