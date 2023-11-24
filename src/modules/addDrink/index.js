575ff7eb94b89c7f57861261b99f75ccd0b13f1c
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-11-24 16:49:58 +0100

    updated popular

--- src/modules/addDrink/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AddForm } from "./components/AddForm/AddForm.jsx";
-export { default as Ingredients } from "./components/Ingredients/Ingredients.jsx";
-export { default as RecipePreparation } from "./components/RecipePreparation/RecipePreparation.jsx";
-export { default as PopularDrinks } from "./components/PopularDrinks/PopularDrinks.jsx";
--- src/shared/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Layout } from "./Layout/Layout";
-export { default as Modal } from "./Modal/Modal";
-export { default as Loader } from "./Loader/Loader";
-export { default as Navbar } from "./Navbar/Navbar";
