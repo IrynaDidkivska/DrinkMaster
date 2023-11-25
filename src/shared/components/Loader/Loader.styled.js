5503f95fd1a626b19a0380255cc407f0adf58ad1
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-11-25 12:26:26 +0100

    updated notadd, li

--- src/shared/components/Loader/Loader.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledLoaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: ${(props) => (props.section === "request" ? "90px" : "100vh")};
-`;
