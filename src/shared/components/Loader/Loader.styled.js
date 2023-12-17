d67c6a6a26b3716299fdf83d3ca67325e90d73c3
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2023-12-17 17:29:31 +0100

    updated loader

--- src/shared/components/Loader/Loader.styled.js
+++ src/shared/components/Loader/Loader.styled.js
@@ -6,14 +6,3 @@ export const StyledLoaderWrapper = styled.div`
   align-items: center;
   height: ${props => (props.section === 'request' ? '90px' : '100vh')};
 `;
-
-export const StyledWrapper = styled.ul`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 12px;
-  height: 100vh;
-  text-align: center;
-  list-style: none;
-`;
